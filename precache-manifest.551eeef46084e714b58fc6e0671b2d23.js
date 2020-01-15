7614ccc977f7e2a37c5eb00f84a2c234edcee06c
michael <mikeyjsalmon@gmail.com>
2020-01-15 14:04:02 -0600

    Updates

---
+++ precache-manifest.551eeef46084e714b58fc6e0671b2d23.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27752ed0dfbb88f8e7662774fe66e40e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "251a32d9e817d0db6161",
+    "url": "/static/css/main.3349e287.chunk.css"
+  },
+  {
+    "revision": "f6642c0e7726632198b9",
+    "url": "/static/js/2.02e1047f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.02e1047f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "251a32d9e817d0db6161",
+    "url": "/static/js/main.06e574be.chunk.js"
+  },
+  {
+    "revision": "467e0774fa58d5ba16da",
+    "url": "/static/js/runtime-main.fd31f462.js"
+  },
+  {
+    "revision": "0f9981f78ab73e8892883d9ffe841b5e",
+    "url": "/static/media/shyguy.0f9981f7.gif"
+  }
+]);
