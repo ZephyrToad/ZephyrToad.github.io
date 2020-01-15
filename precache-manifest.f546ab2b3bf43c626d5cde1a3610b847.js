e7715905578d5b0d908b47941d43ab6c426a8e15
michael <mikeyjsalmon@gmail.com>
2020-01-15 14:36:58 -0600

    Updates

---
+++ precache-manifest.f546ab2b3bf43c626d5cde1a3610b847.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a267fa72557a7bd37ad7da450ae1cf51",
+    "url": "/stay-awake/index.html"
+  },
+  {
+    "revision": "ce88e545d5c919d3982f",
+    "url": "/stay-awake/static/css/main.3349e287.chunk.css"
+  },
+  {
+    "revision": "f6642c0e7726632198b9",
+    "url": "/stay-awake/static/js/2.02e1047f.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/stay-awake/static/js/2.02e1047f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ce88e545d5c919d3982f",
+    "url": "/stay-awake/static/js/main.ca11f6a8.chunk.js"
+  },
+  {
+    "revision": "99531394d7077cd2cc12",
+    "url": "/stay-awake/static/js/runtime-main.1b95184e.js"
+  },
+  {
+    "revision": "0f9981f78ab73e8892883d9ffe841b5e",
+    "url": "/stay-awake/static/media/shyguy.0f9981f7.gif"
+  }
+]);
